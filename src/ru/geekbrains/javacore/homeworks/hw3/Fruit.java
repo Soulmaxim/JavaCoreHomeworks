2cfa259057dc8092ffdbf780a6f298527e39ac22
BorisovMaxim <soulmaxim@gmail.com>
2021-06-07 19:59:19 +0400

    Create hw3

--- src/ru/geekbrains/javacore/homeworks/hw3/Fruit.java
+++ src/ru/geekbrains/javacore/homeworks/hw3/Fruit.java
@@ -1,4 +1,9 @@
 package ru.geekbrains.javacore.homeworks.hw3;
 
 public class Fruit {
+    protected float weight;
+
+    public float getWeight() {
+        return weight;
+    }
 }
--- src/ru/geekbrains/javacore/homeworks/hw3/Orange.java
+++ src/ru/geekbrains/javacore/homeworks/hw3/Orange.java
@@ -1,4 +1,7 @@
 package ru.geekbrains.javacore.homeworks.hw3;
 
-public class Orange {
+public class Orange extends Fruit {
+    public Orange() {
+        this.weight = 1.5f;
+    }
 }
